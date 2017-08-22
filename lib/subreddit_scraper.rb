0380a700298ba648fa84214387fdfd25bda767d6
Drew Pham <drewvpham@gmail.com>
2017-08-22 15:19:57 -0500

    have environment up and running

--- lib/subreddit_scraper.rb
+++ lib/subreddit_scraper.rb
@@ -2,6 +2,5 @@ require 'nokogiri'
 require 'open-uri'
 
 require_relative './subreddit_scraper/version'
-require_relative './subreddit_scraper/cli'
-require_relative './subreddit_scraper/meetup'
 require_relative './subreddit_scraper/scraper'
+require_relative './subreddit_scraper/cli'
