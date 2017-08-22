4646bd4c6fbbbb25c54f885033b3426b35d02d23
Drew Pham <drewvpham@gmail.com>
2017-08-22 15:04:20 -0500

    first commit

---
+++ lib/subreddit_scraper.rb
@@ -0,0 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+
+require_relative './subreddit_scraper/version'
+require_relative './subreddit_scraper/cli'
+require_relative './subreddit_scraper/meetup'
+require_relative './subreddit_scraper/scraper'
---
+++ lib/subreddit_scraper/version.rb
@@ -0,0 +1,3 @@
+module SubredditScraper
+  VERSION = "0.1.0"
+end
